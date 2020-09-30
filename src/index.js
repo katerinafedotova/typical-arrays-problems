a3e4c82383d5d73882ccc33990ec7429df4a7e27
Ekaterina <fedotova490@gmail.com>
2020-09-30 20:36:41 +0300

    feat: implement 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) { 
+  if (!array ||!array.length) 
+  return 0
+  let minElement = Math.min.apply(Math,array)
+  return minElement
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array ||!array.length) 
+  return 0
+  let maxElement = Math.max.apply(Math,array)
+  return maxElement
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+  if (!array || !array.length) 
+  return 0
+  for (let i = 0; i < array.length; i++) {
+    result += array[i]
+  }
+  let avgValue = result / array.length
+  return avgValue
 }
